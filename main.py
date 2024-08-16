62179a65549f8d3a776bc7c0b94754eba097aa89
hheunen <45571110+hheunen@users.noreply.github.com>
2024-08-16 22:34:55 +0200

    arg parse

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-# main.py
+import os
+os.environ["KIVY_NO_ARGS"] = "1"
 
+import argparse
 from main_app import MainApp
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Application de gestion des animaux et soins",
+        usage="%(prog)s [-h] -L"
+    )
+
+    # Ajout de l'argument -L pour lancer l'application
+    parser.add_argument('-L', '--launch', action='store_true', help="Lancer l'application")
+
+    args = parser.parse_args()
+
+    if not args.launch:
+        parser.print_help()
+        exit(1)
+    
+    return args
+
 if __name__ == '__main__':
-    MainApp().run()
+    args = parse_args()
+
+    if args.launch:
+        MainApp().run()
